08e71700b09488834aed6fa99b875ba96296044d
kloudtaxi <kloudstudio@outlook.com>
2021-04-19 12:46:27 -0400

    k9o-demo-app customized to work with ks-platform

---
+++ demo-app/marketplace-demo-app/source/app/src/components/GlobalHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="global-header text-center">
+    <img
+      :src="require('@/assets/k9o-logo.svg')"
+      alt="K9O Marketplace Logo"
+      class="mx-auto"
+    />
+  </header>
+</template>
